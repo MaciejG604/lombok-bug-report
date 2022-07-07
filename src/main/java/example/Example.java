cd1bd4a9d19a19ef0e72e507f4dee207155ff79c
Maciej Gajek <mgajek@virtuslab.com>
2022-07-07 13:21:25 +0200

    Added test case for simple object and non-template interface

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -4,30 +4,30 @@ import lombok.experimental.ExtensionMethod;
 
 import java.util.ArrayList;
 
-@ExtensionMethod({Ext.class, Try.class})
+@ExtensionMethod({Ext.class, Try.class, TrySecond.class, TryThird.class})
 public class Example {
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>();
+        Integer integer = 1;
+
+        array.extensionExt();
 
 //        error: cannot find symbol array.extensionTry();
 //        array.extensionTry();
 
-//        error: cannot find symbol array.extensionTryInteger();
-//        array.extensionTryInteger();
+//        error: cannot find symbol array.extensionTrySecond();
+//        array.extensionTrySecond();
 
-        array.extensionExt();
-        array.extensionExtInteger();
+//        error: cannot find symbol integer.extensionTryThird();
+//        integer.extensionTryThird();
     }
 }
 
 class Ext {
     public static <T> void extensionExt(Iterable<? extends T> values) {
         System.out.println("extensionExt");
-        //Try.extensionTry(values);
-    }
-    public static void extensionExtInteger(Iterable<Integer> values) {
-        System.out.println("extensionExtInteger");
-        //Try.extensionTry(values);
+        //Possible workaround:
+        Try.extensionTry(values);
     }
 }
 
@@ -35,8 +35,16 @@ interface Try<T> {
     static <T> void extensionTry(Iterable<? extends T> values) {
         System.out.println("extensionTry");
     }
+}
 
-    static void extensionTryInteger(Iterable<Integer> values) {
-        System.out.println("extensionTryInteger");
+interface TrySecond {
+    static void extensionTrySecond(Iterable<Integer> values) {
+        System.out.println("extensionTrySecond");
+    }
+}
+
+interface TryThird {
+    static void extensionTryThird(Integer value) {
+        System.out.println("extensionTryThird");
     }
 }
